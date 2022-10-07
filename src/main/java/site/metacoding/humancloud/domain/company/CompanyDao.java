99b298d55e290abc3ef8733e9014ef329a9682bb
gitthathonor <jeong.suyoung92@gmail.com>
2022-10-07 15:33:37 +0900

    company회원가입시, ajax로 데이터 입력 성공

--- src/main/java/site/metacoding/humancloud/domain/company/CompanyDao.java
+++ src/main/java/site/metacoding/humancloud/domain/company/CompanyDao.java
@@ -2,8 +2,10 @@ package site.metacoding.humancloud.domain.company;
 
 import java.util.List;
 
+import site.metacoding.humancloud.web.dto.request.company.JoinDto;
+
 public interface CompanyDao {
-	public void save(Company company);
+	public void save(JoinDto joinDto);
 	public Company findById(Integer id);
 	public List<Company> findAll();
 	public void update(Company company);
--- src/main/java/site/metacoding/humancloud/service/CompanyService.java
+++ src/main/java/site/metacoding/humancloud/service/CompanyService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import lombok.RequiredArgsConstructor;
 import site.metacoding.humancloud.domain.category.Category;
 import site.metacoding.humancloud.domain.category.CategoryDao;
+import site.metacoding.humancloud.domain.company.Company;
 import site.metacoding.humancloud.domain.company.CompanyDao;
 import site.metacoding.humancloud.web.dto.request.company.JoinDto;
 
@@ -19,17 +20,12 @@ public class CompanyService {
 	// 기업 회원 가입
 	public void joinCompany(JoinDto joinDto) {
 		// 기업 정보는 기업 테이블에 저장
-		companyDao.save(joinDto.toEntity());
+		companyDao.save(joinDto);
+		System.out.println(joinDto.getCompanyId());
 		for (String category : joinDto.getCategoryList()) {
 			Category categoryElement = new Category(joinDto.getCompanyId(), category);
 			categoryDao.save(categoryElement);
 		}
 		
 	}
-
-	// 기업 회원 가입 주소 테스트
-	public void saveCompany(JoinDto joinDto) {
-		companyDao.save(joinDto.toEntity());
-	}
-
 }
